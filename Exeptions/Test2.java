525afc8b7ad2ccff2df065f0db96fa2c4ef5828a
GundA99 <akashgund1999@gmail.com>
2023-10-25 00:38:27 +0530

    InterruptedException

---
+++ Exeptions/Test2.java
@@ -0,0 +1,36 @@
+package Tests;
+
+public class Test2 {
+
+	/**************************************************/
+	/*
+	public static void main(String[] args) {
+		
+		System.out.println("Before....");
+		try
+		{
+			Thread.sleep(5000);
+		}
+		catch( InterruptedException e)
+		{
+			System.out.println("Hi");
+		}
+		System.out.println("After....");
+	
+	/****************************************************/
+		
+		public static void main(String[] args) throws InterruptedException
+		{
+			
+			System.out.println("Before....");
+		
+				Thread.sleep(5000);
+			
+			System.out.println("After....");	
+	
+	
+	
+	
+	}
+
+}
